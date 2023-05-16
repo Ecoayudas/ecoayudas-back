43dee55dd838d86ed59b592ad294604219570080
Reyes Bellido Samaniego <reyes.bellidosamaniego@plexus.com>
2023-05-16 21:45:39 +0200

    "api lista para proyecto y securizada faltan test"

---
+++ src/test/java/com/numerus/ecoayudas/v1/app/controller/InstaladorControllerTest.java
@@ -0,0 +1,29 @@
+package com.numerus.ecoayudas.v1.app.controller;
+
+import com.numerus.ecoayudas.v1.app.impl.InstaladorServiceImpl;
+import com.numerus.ecoayudas.v1.app.model.Instalador;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+
+public class InstaladorControllerTest {
+
+    @Mock
+    private InstaladorServiceImpl instaladorServiceImpl;
+    @InjectMocks
+    private InstaladorController instaladorController;
+    @Test
+    void createInstalador(){
+        Instalador instalador = new Instalador();
+        instalador.setId(1L);
+        instalador.setNombre("Empresa");
+        instalador.setDni("12345678L");
+        instaladorController.save(instalador);
+        Mockito.verify(instaladorServiceImpl).save(instalador);
+    }
+}
