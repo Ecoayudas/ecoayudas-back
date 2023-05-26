240eca88e7bbcd4d1513435ead5f08a905c5ff65
Reyes Bellido Samaniego <reyes.bellidosamaniego@plexus.com>
2023-05-26 17:50:48 +0200

    Eliminación de archivos duplicados

--- ecoayudas/ecoayudas/src/main/java/com/numerus/ecoayudas/v1/app/repository/EstadoSolicitudRepository.java
+++
@@ -1,7 +0,0 @@
-package com.numerus.ecoayudas.v1.app.repository;
-
-import com.numerus.ecoayudas.v1.app.model.EstadoSolicitud;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EstadoSolicitudRepository extends JpaRepository<EstadoSolicitud, Long> {
-}
--- ecoayudas/ecoayudas/src/main/java/com/numerus/ecoayudas/v1/app/repository/SolicitudRepository.java
+++
@@ -1,7 +0,0 @@
-package com.numerus.ecoayudas.v1.app.repository;
-
-import com.numerus.ecoayudas.v1.app.model.Solicitud;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SolicitudRepository extends JpaRepository <Solicitud, Long> {
-}
